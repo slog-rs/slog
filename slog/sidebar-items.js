9a6cffeeac9e318737117f39e6556da26a444641
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-06-12 14:57:11 -0800

    Update documentation

--- slog/sidebar-items.js
+++ slog/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Level","Logging level"]],"mod":[["drain","Drains - logging outputs # Drains"],["logger","Logger - logging handle # Logger"]],"struct":[["RecordBuilder",""],["RecordInfo",""]]});
+initSidebarItems({"enum":[["Level","Logging level"]],"mod":[["drain","Drains - logging outputs # Drains"],["logger","Logger - logging handle # Logger"]],"struct":[["RecordBuilder","Build logging record"],["RecordInfo","Common information about a logging record"]]});
