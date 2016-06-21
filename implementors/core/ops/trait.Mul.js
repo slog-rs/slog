3ccc6627a3f300c313f5047489b685c81668a210
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-06-20 23:32:15 -0800

    Update documentation

--- implementors/core/ops/trait.Mul.js
+++ implementors/core/ops/trait.Mul.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['serde'] = [];
+implementors['libc'] = [];implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Mul.html' title='core::ops::Mul'>Mul</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.i32.html'>i32</a>&gt; for <a class='struct' href='time/struct.Duration.html' title='time::Duration'>Duration</a>",];implementors['chrono'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Mul.html' title='core::ops::Mul'>Mul</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.i32.html'>i32</a>&gt; for <a class='struct' href='chrono/duration/struct.Duration.html' title='chrono::duration::Duration'>Duration</a>",];implementors['serde'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/ops/trait.Neg.js
+++ implementors/core/ops/trait.Neg.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['serde'] = [];
+implementors['libc'] = [];implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='time/struct.Duration.html' title='time::Duration'>Duration</a>",];implementors['chrono'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='chrono/duration/struct.Duration.html' title='chrono::duration::Duration'>Duration</a>",];implementors['serde'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
