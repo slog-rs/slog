847433c0ba285b58647346c5e25e4a8423e5115c
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-06-25 23:19:50 -0800

    Update documentation

---
+++ implementors/std/io/trait.Write.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['slog'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='slog/drain/struct.AsyncIoWriter.html' title='slog::drain::AsyncIoWriter'>AsyncIoWriter</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
