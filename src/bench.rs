81311b92aaf3d66199facbc4c72cd38a82d7acb8
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-07-21 12:08:54 -0700

    Forgotten files

---
+++ src/bench.rs
@@ -0,0 +1,57 @@
+use test::Bencher;
+use super::*;
+
+#[bench]
+fn empty_log_discard(b: &mut Bencher) {
+    let log = Logger::new_root(o!());
+
+    b.iter(|| {
+        log.info("", b!());
+    });
+}
+
+#[bench]
+fn nonempty_log_discard(b: &mut Bencher) {
+    let log = Logger::new_root(o!("build" => "123456", "id" => 123456));
+
+    b.iter(|| {
+        log.info("operation failed", b!("what" => "write"));
+    });
+}
+
+#[bench]
+fn empty_logger_clone(b: &mut Bencher) {
+    let log = Logger::new_root(o!());
+
+    b.iter(|| {
+        log.clone()
+    });
+}
+
+#[bench]
+fn nonempty_logger_clone(b: &mut Bencher) {
+    let log = Logger::new_root(o!("build" => "123456", "id" => 123456));
+
+    b.iter(|| {
+        log.clone()
+    });
+}
+
+#[bench]
+fn empty_logger_new(b: &mut Bencher) {
+    let log = Logger::new_root(o!());
+
+    b.iter(|| {
+        log.new(o!())
+    });
+}
+
+
+#[bench]
+fn nonempty_logger_new(b: &mut Bencher) {
+    let log = Logger::new_root(o!("build" => "123456", "id" => 123456));
+
+    b.iter(|| {
+        log.new(o!("what" => "write"));
+    });
+}
