6444544e82477761bf25df70af1c7b36779c72c2
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-08-02 21:54:27 -0700

    Add `alternative_names.rs` example

---
+++ examples/alternative_names.rs
@@ -0,0 +1,17 @@
+#[macro_use(o,slog_log,slog_trace,slog_debug,slog_info,slog_warn,slog_error,slog_crit)]
+extern crate slog;
+
+extern crate slog_term;
+
+use slog::drain::IntoLogger;
+
+fn main() {
+    let log = slog_term::stderr().into_logger(o!());
+
+    slog_trace!(log, "logging a trace message");
+    slog_debug!(log, "debug values", "x" => 1, "y" => -1);
+    slog_info!(log, "some interesting info", "where" => "right here");
+    slog_warn!(log, "be cautious!", "why" => "you never know...");
+    slog_error!(log, "something's wrong", "type" => "unknown");
+    slog_crit!(log, "abandoning test");
+}
