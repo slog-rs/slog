41fe4fded8131ec314ca27aac429e04103a7b963
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-08-12 23:28:38 -0700

    Add `example/file-line-module`
    Fixes #27

---
+++ examples/file-line-module.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate slog;
+extern crate slog_json;
+extern crate slog_term;
+
+use slog::*;
+use slog::drain::{IntoLogger};
+
+fn main() {
+    let drain = slog_term::async_stderr();
+    let log = drain.into_logger(o!("place" =>
+        move |info : &RecordInfo| {
+            format!("{}:{} {}",
+                    info.file(),
+                    info.line(),
+                    info.module(),
+            )
+        }
+    ));
+
+    debug!(log, "HERE");
+}
