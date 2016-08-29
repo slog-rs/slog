f35cd19421f8447f34e416df20da4023e9553c5d
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-08-28 16:29:15 -0800

    Update documentation

--- implementors/slog/trait.Drain.js
+++ implementors/slog/trait.Drain.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["slog"] = [];implementors["slog_syslog"] = ["impl <a class='trait' href='slog/trait.Drain.html' title='slog::Drain'>Drain</a> for <a class='struct' href='slog_syslog/struct.Streamer3164.html' title='slog_syslog::Streamer3164'>Streamer3164</a>",];
+implementors["slog"] = [];implementors["slog_syslog"] = ["impl <a class='trait' href='slog/trait.Drain.html' title='slog::Drain'>Drain</a> for <a class='struct' href='slog_syslog/struct.Streamer3164.html' title='slog_syslog::Streamer3164'>Streamer3164</a>",];implementors["slog_stdlog"] = ["impl <a class='trait' href='slog/trait.Drain.html' title='slog::Drain'>Drain</a> for <a class='struct' href='slog_stdlog/struct.StdLog.html' title='slog_stdlog::StdLog'>StdLog</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
