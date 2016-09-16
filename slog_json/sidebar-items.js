0ca483e1001686af3bafcbe3f9bc95203002719b
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-09-15 22:20:44 -0800

    Update documentation

--- slog_json/sidebar-items.js
+++ slog_json/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["build","Short for `FormatBuilder::build()`"],["new","Short for `Format::new()`"]],"struct":[["Format","Json formatter"],["FormatBuilder","Json formatter builder"]]});
+initSidebarItems({"fn":[["build","Use builder to create a custom Json formatter"],["new","Build a default Json formatter"]],"struct":[["Format","Json formatter"],["FormatBuilder","Json formatter builder"]]});
