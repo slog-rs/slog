55148b2505f7d9fb77b0136e2ff751ec17e368db
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-09-17 19:02:22 -0800

    Update documentation

--- implementors/slog/ser/trait.Serializer.js
+++ implementors/slog/ser/trait.Serializer.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["slog_serde"] = ["impl&lt;'a,&nbsp;S&gt; <a class='trait' href='slog/ser/trait.Serializer.html' title='slog::ser::Serializer'>Serializer</a> for <a class='struct' href='slog_serde/struct.SerdeSerializer.html' title='slog_serde::SerdeSerializer'>SerdeSerializer</a>&lt;'a,&nbsp;S&gt; <span class='where'>where S: 'a + <a class='trait' href='serde/ser/trait.Serializer.html' title='serde::ser::Serializer'>Serializer</a></span>",];
+implementors["slog_serde"] = ["impl&lt;S&gt; <a class='trait' href='slog/ser/trait.Serializer.html' title='slog::ser::Serializer'>Serializer</a> for <a class='struct' href='slog_serde/struct.SerdeSerializer.html' title='slog_serde::SerdeSerializer'>SerdeSerializer</a>&lt;S&gt; <span class='where'>where S: <a class='trait' href='serde/ser/trait.Serializer.html' title='serde::ser::Serializer'>Serializer</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
