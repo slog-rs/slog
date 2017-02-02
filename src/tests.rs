7847986a1909541214d4179bbe36e18a8d03397d
Dawid Ciężarkiewicz <dpc@dpc.pw>
2017-02-01 22:10:31 -0800

    Fix tests after owned values ordering change

--- src/tests.rs
+++ src/tests.rs
@@ -6,7 +6,7 @@ fn logger_fmt_debug_sanity() {
     let root = Logger::root(Discard, o!("a" => "aa"));
     let log = root.new(o!("b" => "bb", "c" => "cc"));
 
-    assert_eq!(format!("{:?}", log), "Logger(b, c, a)");
+    assert_eq!(format!("{:?}", log), "Logger(c, b, a)");
 }
 
 #[cfg(test)]
