5d1f60ae1a21901722bc345cd5b8ba316a5606b2
Andreas Fuchs <asf@boinkor.net>
2020-03-12 19:04:12 -0400

    impl PartialEq<&str> for dynamic::Key also
    This makes the (non-doctest) tests with --features dynamic-keys pass.

--- src/key/dynamic_nostd.rs
+++ src/key/dynamic_nostd.rs
@@ -126,6 +126,13 @@ impl PartialEq<str> for Key {
     }
 }
 
+impl PartialEq<&str> for Key {
+    #[inline(always)]
+    fn eq(&self, other: &&str) -> bool {
+        self.as_ref().eq(*other)
+    }
+}
+
 impl PartialEq<String> for Key {
     #[inline(always)]
     fn eq(&self, other: &String) -> bool {
